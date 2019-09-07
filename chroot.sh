2bee7ce5b1b269a211d8f7f73a9ba1ab2b47e07e
poruncov <48706308+poruncov@users.noreply.github.com>
2019-09-07 15:18:18 +0300

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -869,7 +869,6 @@ elif [[ $x_shell == 1 ]]; then
 clear
 pacman -S zsh  zsh-syntax-highlighting zsh-autosuggestions grml-zsh-config --noconfirm
 echo 'source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> /etc/zsh/zshrc
-echo 'source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh' >> /etc/zsh/zshrc
 echo 'prompt adam2' >> /etc/zsh/zshrc
 echo " сменим оболочку пользователя с bash на zsh? "
 while 
