6961ea7f00c5ffb7b041dd42a72177d33318d3b9
poruncov <48706308+poruncov@users.noreply.github.com>
2020-01-13 22:41:20 +0300

    Add files via upload

---
+++ boot.sh
@@ -0,0 +1,288 @@
+#!/bin/bash
+echo " "
+echo "Данный скрипт позволит востановить загрузчик системы "
+echo "после переустановки Windows или каких либо других монипуляций!"
+echo ""
+echo "Востаноим загрузчик ArchLinux"
+ ###################################################################################
+echo ""
+echo 'Есть отдельный UEFI или legacy< Grub >?'
+while 
+    read -n1 -p  "
+    1 - да
+    
+    0 - нет: " disk # sends right after the keypress
+    echo ''
+    [[ "$disk" =~ [^10] ]]
+do
+    :
+done
+if [[ $disk == 1 ]]; then
+clear
+lsblk -f
+echo ""
+read -p "Укажите Root раздел(sda/sdb 1.2.3.4 (sda5 например)):" root
+mount /dev/$root /mnt
+echo ""
+read -p "Укажите Boot раздел(ТОЛЬКО ДЛЯ UEFI )(sda/sdb 1.2.3.4 (sda2 например)):" boot
+mount /dev/$boot /mnt/boot
+read -p "Укажите Home раздел(sda/sdb 1.2.3.4 (sda6 например)):" home
+mount /dev/$home /mnt/home
+######
+
+ clear
+ lsblk -f
+  echo ""
+echo 'добавим swap раздел?'
+while 
+    read -n1 -p  "
+    1 - да
+    
+    0 - нет: " swap # sends right after the keypress
+    echo ''
+    [[ "$swap" =~ [^10] ]]
+do
+    :
+done
+ if [[ $swap == 1 ]]; then
+  read -p "Укажите swap раздел(sda/sdb 1.2.3.4 (sda7 например)):" swaps
+  mkswap /dev/$swaps -L swap
+  swapon /dev/$swaps
+  elif [[ $swap == 0 ]]; then
+   echo 'добавление swap раздела пропущено.'   
+fi
+
+
+######
+clear
+echo 'Добавим разделы  Windows (ntfs/fat32)?'
+while 
+    read -n1 -p  "
+    1 - да
+    
+    0 - нет: " wind # sends right after the keypress
+    echo ''
+    [[ "$wind" =~ [^10] ]]
+do
+    :
+done
+if [[ $wind == 0 ]]; then
+  echo 'пропущено'
+  elif [[ $wind == 1 ]]; then
+  echo "#####################################################################################"
+echo ""
+  echo 'Добавим раздел диск "C" Windows?'
+while 
+    read -n1 -p  "
+    1 - да
+    
+    0 - нет: " diskC # sends right after the keypress
+    echo ''
+    [[ "$diskC" =~ [^10] ]]
+do
+    :
+done
+if [[ $diskC == 0 ]]; then
+  echo 'пропущено'
+  elif [[ $diskC == 1 ]]; then
+   clear
+ lsblk -f
+  echo ""
+  read -p " Укажите диск "C" раздел(sda/sdb 1.2.3.4 (sda4 например) ) : " diskCc
+  mkdir /mnt/C 
+  mount /dev/$diskCc /mnt/C
+  fi
+############### disk D ##############
+echo 'Добавим раздел диск "D" Windows?'
+while 
+    read -n1 -p  "
+    1 - да
+    
+    0 - нет: " diskD # sends right after the keypress
+    echo ''
+    [[ "$diskD" =~ [^10] ]]
+do
+    :
+done
+if [[ $diskD == 1 ]]; then
+ clear
+ lsblk -f
+  echo ""
+  read -p " Укажите диск "D" раздел(sda/sdb 1.2.3.4 (sda5 например)) : " diskDd
+  mkdir /mnt/D 
+  mount /dev/$diskDd /mnt/D
+  elif [[ $diskD == 0 ]]; then
+  echo 'пропущено'
+  fi
+###### disk E ########
+echo 'Добавим раздел диск "E" Windows?'
+while 
+    read -n1 -p  "
+    1 - да
+    
+    0 - нет: " diskE  # sends right after the keypress
+    echo ''
+    [[ "$diskE" =~ [^10] ]]
+do
+    :
+done
+ if [[ $diskE == 1 ]]; then
+  clear
+ lsblk -f
+  echo ""
+  read -p " Укажите диск "E" раздел(sda/sdb 1.2.3.4 (sda5 например)) : " diskDe
+  mkdir /mnt/E 
+  mount /dev/$diskDe /mnt/E
+  elif [[ $diskE == 0 ]]; then
+  echo 'пропущено'
+  fi 
+  fi
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt  sh -c "$(curl -fsSL https://raw.githubusercontent.com/poruncov/archlinux-script-install/master/chroot.sh)"
+reboot
+######
+elif [[ $disk == 0 ]]; then
+ lsblk -f
+echo ""
+read -p "Укажите Root раздел(sda/sdb 1.2.3.4 (sda4 например)):" root
+mount /dev/$root /mnt
+echo ""
+#######
+echo 'Добавим раздел Boot, если нету тогда не указываем!!!!!'
+while 
+    read -n1 -p  "
+    1 - да
+    
+    0 - нет: " boot # sends right after the keypress
+    echo ''
+    [[ "$boot" =~ [^10] ]]
+do
+    :
+done
+if [[ $diskD == 1 ]]; then
+ clear
+ lsblk -f
+  echo ""
+  read -p " Укажите раздел boot (sda/sdb 1.2.3.4 (sda1 например)) : " boot
+  mount /dev/$boot /mnt/boot
+  elif [[ $boot == 0 ]]; then
+  echo 'пропущено'
+  fi
+#######
+read -p "Укажите Home раздел(sda/sdb 1.2.3.4 (sda5 например)):" home
+mount /dev/$home /mnt/home 
+##########################################################
+clear
+lsblk -f
+echo ""
+echo 'добавим swap раздел?'
+while 
+    read -n1 -p  "
+    1 - да
+    
+    0 - нет: " swap # sends right after the keypress
+    echo ''
+    [[ "$swap" =~ [^10] ]]
+do
+    :
+done
+ if [[ $swap == 1 ]]; then
+  read -p "Укажите swap раздел(sda/sdb 1.2.3.4 (sda7 например)):" swaps
+  mkswap /dev/$swaps -L swap
+  swapon /dev/$swaps
+  elif [[ $swap == 0 ]]; then
+   echo 'добавление swap раздела пропущено.'   
+fi
+#####
+clear
+echo 'Добавим разделы  Windows (ntfs/fat32)?'
+while 
+    read -n1 -p  "
+    1 - да
+    
+    0 - нет: " wind # sends right after the keypress
+    echo ''
+    [[ "$wind" =~ [^10] ]]
+do
+    :
+done
+if [[ $wind == 0 ]]; then
+  echo 'пропущено'
+  elif [[ $wind == 1 ]]; then
+  echo "#####################################################################################"
+echo ""
+  echo 'Добавим раздел диск "C" Windows?'
+while 
+    read -n1 -p  "
+    1 - да
+    
+    0 - нет: " diskC # sends right after the keypress
+    echo ''
+    [[ "$diskC" =~ [^10] ]]
+do
+    :
+done
+if [[ $diskC == 0 ]]; then
+  echo 'пропущено'
+  elif [[ $diskC == 1 ]]; then
+   clear
+ lsblk -f
+  echo ""
+  read -p " Укажите диск "C" раздел(sda/sdb 1.2.3.4 (sda4 например) ) : " diskCc
+  mkdir /mnt/C 
+  mount /dev/$diskCc /mnt/C
+  fi
+############### disk D ##############
+echo 'Добавим раздел диск "D" Windows?'
+while 
+    read -n1 -p  "
+    1 - да
+    
+    0 - нет: " diskD # sends right after the keypress
+    echo ''
+    [[ "$diskD" =~ [^10] ]]
+do
+    :
+done
+if [[ $diskD == 1 ]]; then
+ clear
+ lsblk -f
+  echo ""
+  read -p " Укажите диск "D" раздел(sda/sdb 1.2.3.4 (sda5 например)) : " diskDd
+  mkdir /mnt/D 
+  mount /dev/$diskDd /mnt/D
+  elif [[ $diskD == 0 ]]; then
+  echo 'пропущено'
+  fi
+###### disk E ########
+echo 'Добавим раздел диск "E" Windows?'
+while 
+    read -n1 -p  "
+    1 - да
+    
+    0 - нет: " diskE  # sends right after the keypress
+    echo ''
+    [[ "$diskE" =~ [^10] ]]
+do
+    :
+done
+ if [[ $diskE == 1 ]]; then
+  clear
+ lsblk -f
+  echo ""
+  read -p " Укажите диск "E" раздел(sda/sdb 1.2.3.4 (sda5 например)) : " diskDe
+  mkdir /mnt/E 
+  mount /dev/$diskDe /mnt/E
+  elif [[ $diskE == 0 ]]; then
+  echo 'пропущено'
+  fi 
+  fi
+genfstab -pU /mnt >> /mnt/etc/fstab
+arch-chroot /mnt  sh -c "$(curl -fsSL https://raw.githubusercontent.com/poruncov/archlinux-script-install/master/chroot.sh)" 
+reboot
+fi
+
+
+
+
+
---
+++ boot2.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+echo ""
+echo "Теперь выбирите, что вам подходит"
+while 
+    read -n1 -p  "
+    1 - UEFI(systemd-boot )
+  
+    2 - GRUB(legacy)
+    
+    3 - UEFI-GRUB: " t_bootloader # sends right after the keypress
+    
+    echo ''
+    [[ "$t_bootloader" =~ [^123] ]]
+do
+    :
+done
+if [[ $t_bootloader == 1 ]]; then
+cd /boot
+ls | grep -v EFI | xargs rm -rfv
+cd /boot/EFI
+ls | grep -v Boot | grep -v Microsoft | xargs rm -rfv
+pacman -S linux
+bootctl install 
+clear
+echo ' default arch ' > /boot/loader/loader.conf
+echo ' timeout 10 ' >> /boot/loader/loader.conf
+echo ' editor 0' >> /boot/loader/loader.conf
+echo 'title   Arch Linux' > /boot/loader/entries/arch.conf
+echo "linux  /vmlinuz-linux" >> /boot/loader/entries/arch.conf
+echo ""
+echo " Добавим ucode cpu? "
+while 
+    read -n1 -p  "
+    1 - amd  
+    
+    2 - intel
+    
+    0 - ucode не добавляем : " i_cpu   
+    echo ''
+    [[ "$i_cpu" =~ [^120] ]]
+do
+    :
+done
+if [[ $i_cpu == 0 ]]; then
+clear
+echo " Добавление ucode пропущено "
+elif [[ $i_cpu  == 1 ]]; then
+clear
+pacman -S amd-ucode --noconfirm
+echo  'initrd /amd-ucode.img ' >> /boot/loader/entries/arch.conf
+elif [[ $i_cpu  == 2 ]]; then
+clear
+pacman -S intel-ucode  --noconfirm
+echo ' initrd /intel-ucode.img ' >> /boot/loader/entries/arch.conf
+fi
+echo "initrd  /initramfs-linux.img" >> /boot/loader/entries/arch.conf
+clear
+lsblk -f
+echo ""
+echo " Укажите тот радел который будет после перезагрузки, то есть например "
+
+echo " при установке с флешки ваш hdd может быть sdb, а после перезагрузки sda "
+
+echo " выше видно что sdbX например примонтирован в /mnt, а после перезагрузки systemd будет искать корень на sdaX "
+
+echo " если указать не правильный раздел система не загрузится "
+
+echo " если у вас один hdd/ssd тогда это будет sda 99%"
+echo ""
+read -p "Укажите ROOT  раздел для загрузчика(пример  sda6,sdb3 ): " root
+echo options root=/dev/$root rw >> /boot/loader/entries/arch.conf
+#
+cd /home/$username 
+git clone https://aur.archlinux.org/systemd-boot-pacman-hook.git
+chown -R $username:users /home/$username/systemd-boot-pacman-hook   
+chown -R $username:users /home/$username/systemd-boot-pacman-hook/PKGBUILD 
+cd /home/$username/systemd-boot-pacman-hook   
+sudo -u $username makepkg -si --noconfirm  
+rm -Rf /home/$username/systemd-boot-pacman-hook
+cd /home/$username 
+#
+clear
+elif [[ $t_bootloader == 2 ]]; then
+clear
+echo " Востановим загрузчик Grub"
+echo ""
+echo "
+1- МультиБУТ 
+
+2- не мультилиб"
+echo ""
+while 
+    read -n1 -p  "
+    1 - да
+    
+    2 - нет: " i_grub      # sends right after the keypress
+    echo ''
+    [[ "$i_grub" =~ [^12] ]]
+do
+    :
+done
+if [[ $i_grub == 2 ]]; then
+pacman -S grub  os-prober --noconfirm
+lsblk -f
+read -p "Укажите диск куда установить GRUB (sda/sdb): " x_boot
+grub-install /dev/$x_boot
+grub-mkconfig -o /boot/grub/grub.cfg
+echo " установка завершена "
+elif [[ $i_grub == 1 ]]; then
+pacman -S grub --noconfirm
+lsblk -f
+read -p "Укажите диск куда установить GRUB (sda/sdb): " x_boot
+grub-install /dev/$x_boot
+grub-mkconfig -o /boot/grub/grub.cfg
+echo " установка завершена "
+fi  
+elif [[ $t_bootloader == 3 ]]; then
+cd /boot
+ls | grep -v EFI | xargs rm -rfv
+cd /boot/EFI
+ls | grep -v Boot | grep -v Microsoft | xargs rm -rfv
+pacman -S linux grub os-prober --noconfirm
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+fi
+mkinitcpio -p linux
+
+exit
+
+
