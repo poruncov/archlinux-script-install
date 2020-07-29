d212c4bb7fe83ce010f4fd33e5a638b6e0663d1f
poruncov <48706308+poruncov@users.noreply.github.com>
2020-07-29 23:15:36 +0300

    Update base.sh

--- base.sh
+++ base.sh
@@ -433,7 +433,8 @@ do
 done
 if [[ $int == 1 ]]; then
 
-  wget -P /mnt https://raw.githubusercontent.com/poruncov/archlinux-script-install/master/chroot.sh
+  curl -LO https://raw.githubusercontent.com/poruncov/archlinux-script-install/master/chroot.sh
+  mv chroot.sh /mnt
   chmod +x /mnt/chroot.sh
   echo 'первый этап готов ' 
   echo 'ARCH-LINUX chroot' 
@@ -735,8 +736,9 @@ do
 done
 if [[ $int == 1 ]]; then
 
-  wget -P /mnt https://raw.githubusercontent.com/poruncov/archlinux-script-install/master/chroot.sh
-  chmod +x /mnt/chroot.sh 
+  curl -LO https://raw.githubusercontent.com/poruncov/archlinux-script-install/master/chroot.sh
+  mv chroot.sh /mnt
+  chmod +x /mnt/chroot.sh
   echo 'первый этап готов ' 
   echo 'ARCH-LINUX chroot' 
   echo '1. проверь  интернет для продолжения установки в черуте || 2.команда для запуска ./chroot.sh ' 
